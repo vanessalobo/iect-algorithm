7f77c4cec03fa3d65f1548379fd44f9e771eda6c
Vanessa Lobo <vanessa.lobo0211@gmail.com>
2020-11-26 14:58:40 +0530

    cr2 not working

--- iect.cpp
+++ iect.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 //inputs: Prefix P, Stack SOI, minimum support minsup, minimum ratio minR
-void IECT(string P, stack<pair<string, vector<set<int> > > > soi, double minsup, double minR, vector<string> CP){
+void IECT(string P, stack<pair<string, vector<set<int> > > >& soi, double minsup, double minR, vector<string>& CP){
 	while(!soi.empty()) {
 
 		pair<string, vector<set<int> > > tos;
@@ -23,18 +23,19 @@ void IECT(string P, stack<pair<string, vector<set<int> > > > soi, double minsup,
 		}
 
 		vector<set<int> > current=tos.second; //tos transaction
-		pair<string, vector<set<int> > > i2;
+		 //i2;
 
 		int N=current.size(); //N+1
 
 		while(!soi.empty()){ 
 
-			i2=soi.top();
+
+			set<int> vip;
+			pair<string, vector<set<int> > > i2=soi.top();
 			soi.pop();  // ??? do i need to pop or just traverse the stack
 			vector<set<int> > t2=i2.second;
-			set<int> vip;
 
-			auto it= set_intersection(current[0].begin(), current[0].end(), t2[0].begin(), t2[0].end(), back_inserter(vip));
+			auto it= set_intersection(current[0].begin(), current[0].end(), t2[0].begin(), t2[0].end(), inserter(vip, vip.begin()));
 
 			if(vip.size() >= minsup) {
 
@@ -44,7 +45,7 @@ void IECT(string P, stack<pair<string, vector<set<int> > > > soi, double minsup,
 
 				for(int i=1; i<N; i++){
 					set<int> temp;
-					auto it=set_intersection(current[i].begin(), current[i].end(), t2[i].begin(), t2[i].end(), back_inserter(temp));
+					auto it=set_intersection(current[i].begin(), current[i].end(), t2[i].begin(), t2[i].end(), inserter(temp, temp.begin()));
 					v.push_back(temp);
 				}
 
@@ -61,5 +62,25 @@ void IECT(string P, stack<pair<string, vector<set<int> > > > soi, double minsup,
 
 
 	}
+}
+
+int main() {
+
+	string P="";
+	stack<pair<string, vector<set<int> > > > soi;
+	soi.push({"a", { {1, 2, 3, 4, 5, 6}, {7}, {9} } });
+	//soi.push({"f", { {1, 2, 4, 5, 6}, {8}, {9} } });
+	//soi.push({"g", { {1, 2, 3, 4, 5}, {7, 8}, {} } });
+	soi.push({"b", { {2, 3, 4, 6}, {7, 8}, {} } }); 
+	double minsup=4.0;
+	double minR=0.4;
+	vector<string> CP;
+
+	IECT(P, soi, minsup, minR, CP);
+
+	for(int i=0; i<CP.size(); i++){
+		cout<<CP[i];
+	}
+	return 0;
 }
 
