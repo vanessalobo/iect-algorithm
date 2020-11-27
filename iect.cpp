2bb361770f74401902774d13a8d5232c40c15d48
Vanessa Lobo <vanessa.lobo0211@gmail.com>
2020-11-27 13:01:41 +0530

    gf not working

--- iect.cpp
+++ iect.cpp
@@ -7,17 +7,20 @@
 using namespace std;
 
 //inputs: Prefix P, Stack SOI, minimum support minsup, minimum ratio minR
-void IECT(string P, stack<pair<string, vector<set<int> > > >& soi, double minsup, double minR, vector<string>& CP){
-	while(!soi.empty()) {
+void IECT(string P, vector<pair<string, vector<set<int> > > >& soi, double minsup, double minR, vector<string>& CP){
+
+	while(soi.size() > 0) {
 
 		pair<string, vector<set<int> > > tos;
-		tos=soi.top();
-		soi.pop();
+		tos=soi.back();
+		soi.pop_back();
 
-		stack<pair<string, vector<set<int> > > > suffix;
+		vector<pair<string, vector<set<int> > > > suffix;
+
+		string X= P+tos.first;
 
 		if((criteriaRatioOne(tos.first, tos.second) <= minR) && criteriaRatioTwo(tos.first, tos.second)){
-			P+=tos.first; //????
+			P=X; //????
 			CP.push_back(P);
 
 		}
@@ -27,15 +30,14 @@ void IECT(string P, stack<pair<string, vector<set<int> > > >& soi, double minsup
 
 		int N=current.size(); //N+1
 
-		while(!soi.empty()){ 
-
+		for(auto it=soi.rbegin(); it!=soi.rend(); it++){ 
 
 			set<int> vip;
-			pair<string, vector<set<int> > > i2=soi.top();
-			soi.pop();  // ??? do i need to pop or just traverse the stack
+			pair<string, vector<set<int> > > i2=*it;
+			//soi.pop();  // ??? do i need to pop or just traverse the stack
 			vector<set<int> > t2=i2.second;
 
-			auto it= set_intersection(current[0].begin(), current[0].end(), t2[0].begin(), t2[0].end(), inserter(vip, vip.begin()));
+			auto itr= set_intersection(current[0].begin(), current[0].end(), t2[0].begin(), t2[0].end(), inserter(vip, vip.begin()));
 
 			if(vip.size() >= minsup) {
 
@@ -45,20 +47,20 @@ void IECT(string P, stack<pair<string, vector<set<int> > > >& soi, double minsup
 
 				for(int i=1; i<N; i++){
 					set<int> temp;
-					auto it=set_intersection(current[i].begin(), current[i].end(), t2[i].begin(), t2[i].end(), inserter(temp, temp.begin()));
+					auto itr=set_intersection(current[i].begin(), current[i].end(), t2[i].begin(), t2[i].end(), inserter(temp, temp.begin()));
 					v.push_back(temp);
 				}
 
-				suffix.push(make_pair(i2.first, v));
+				suffix.push_back(make_pair(i2.first, v));
 
 			}
 
 
 		}
 
-		P+=tos.first; //????
+		 //????
 		
-		IECT(P, suffix, minsup, minR, CP);
+		IECT(X, suffix, minsup, minR, CP);
 
 
 	}
@@ -67,11 +69,11 @@ void IECT(string P, stack<pair<string, vector<set<int> > > >& soi, double minsup
 int main() {
 
 	string P="";
-	stack<pair<string, vector<set<int> > > > soi;
-	soi.push({"a", { {1, 2, 3, 4, 5, 6}, {7}, {9} } });
-	soi.push({"f", { {1, 2, 4, 5, 6}, {8}, {9} } });
-	soi.push({"g", { {1, 2, 3, 4, 5}, {7, 8}, {} } });
-	//soi.push({"b", { {2, 3, 4, 6}, {7, 8}, {} } }); 
+	vector<pair<string, vector<set<int> > > > soi;
+	soi.push_back({"a", { {1, 2, 3, 4, 5, 6}, {7}, {9} } });
+	soi.push_back({"f", { {1, 2, 4, 5, 6}, {8}, {9} } });
+	soi.push_back({"g", { {1, 2, 3, 4, 5}, {7, 8}, {} } });
+	soi.push_back({"b", { {2, 3, 4, 6}, {7, 8}, {} } }); 
 	double minsup=4.0;
 	double minR=0.4;
 	vector<string> CP;
